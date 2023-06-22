4ceff54725a4c6aa5c267fef335427529694a233
ash-sxn <ash.191245141@gmail.com>
2023-06-22 05:45:16 +0530

    fix:gitpod_permission_issue

--- experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
+++ experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+# Removing existing keys
+rm -f secrets/jenkins_agent_ed secrets/jenkins_agent_ed.pub
 # Generate new ed25519 SSH key pair
 ssh-keygen -t ed25519 -f secrets/jenkins_agent_ed -N ""
+chmod 444 secrets/jenkins_agent_ed
 
 # Extract public key
 pubkey=$(cat secrets/jenkins_agent_ed.pub)
@@ -9,3 +11,5 @@ pubkey=$(cat secrets/jenkins_agent_ed.pub)
 # Update docker-compose file with new public key
 sed -i "s|JENKINS_AGENT_SSH_PUBKEY=.*|JENKINS_AGENT_SSH_PUBKEY=$pubkey|" docker-compose.yaml
 ls
+# Running docker-compose file 
+docker compose up -d 
