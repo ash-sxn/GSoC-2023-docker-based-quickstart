450c74d2e779130483b4e09b1640e0e058001efb
Ashutosh Saxena <98499313+ash-sxn@users.noreply.github.com>
2023-06-22 13:02:06 +0530

    Update experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
    Co-authored-by: Bruno Verachten <gounthar@gmail.com>

--- experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
+++ experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-# Add secrets directory
-mkdir secrets
-# Removing existing keys
+# This script generates a new SSH key pair and updates a Docker Compose file with the public key.
+# Create secrets directory if it doesn't exist
+mkdir -p secrets
+
+# Remove existing keys
 rm -f secrets/jenkins_agent_ed secrets/jenkins_agent_ed.pub
+
 # Generate new ed25519 SSH key pair
 ssh-keygen -t ed25519 -f secrets/jenkins_agent_ed -N ""
-chmod 444 secrets/jenkins_agent_ed
+
+# Set appropriate permissions for private key
+chmod 400 secrets/jenkins_agent_ed
 
 # Extract public key
 pubkey=$(cat secrets/jenkins_agent_ed.pub)
 
 # Update docker-compose file with new public key
 sed -i "s|JENKINS_AGENT_SSH_PUBKEY=.*|JENKINS_AGENT_SSH_PUBKEY=$pubkey|" docker-compose.yaml
+
+# Display success message
+echo "SSH key pair generated and Docker Compose file updated successfully."
+
+# End of script
