cab948eeb45491a3c7d4adbf149d0ee6ae72d4d7
Ashutosh Saxena <98499313+ash-sxn@users.noreply.github.com>
2023-06-23 11:48:47 +0530

    fix: macOs `sed` command issue

--- experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
+++ experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller/keygen.sh
@@ -15,8 +15,12 @@ chmod 404 secrets/jenkins_agent_ed
 # Extract public key
 pubkey=$(cat secrets/jenkins_agent_ed.pub)
 
-# Update docker-compose file with new public key
-sed -i "s|JENKINS_AGENT_SSH_PUBKEY=.*|JENKINS_AGENT_SSH_PUBKEY=$pubkey|" docker-compose.yaml
+# Check OS with if else statement and Update docker-compose file with a new public key with the appropriate command for MacOS(darwin) and Linux (linux-gnu) & windows-wsl(linux-gnu)  
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s|JENKINS_AGENT_SSH_PUBKEY=.*|JENKINS_AGENT_SSH_PUBKEY=$pubkey|" docker-compose.yaml
+    else
+        sed -i "s|JENKINS_AGENT_SSH_PUBKEY=.*|JENKINS_AGENT_SSH_PUBKEY=$pubkey|" docker-compose.yaml
+    fi
 
 # Display success message
 echo "SSH key pair generated and Docker Compose file updated successfully."
