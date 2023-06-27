31b6601b43b6a653ee0afab5860d34dd6d9fed49
ash-sxn <ash.191245141@gmail.com>
2023-06-27 18:27:23 +0530

    feature:init_and_teardown.sh

--- jenkins_init.sh
+++ jenkins_init.sh
@@ -2,91 +2,51 @@
 set -exo pipefail
 
 # Assign the first argument passed to the script to the variable TUTORIAL
-
 TUTORIAL=$1
 
-# Assign values 0-3 to variables VAR0-VAR3
-
-VAR0=0
+# Assign values 1-4 to variables VAR1-VAR4 can be changed in future to maven, nodejs, python
 VAR1=1
 VAR2=2
 VAR3=3
+VAR4=4
 
 # Assign file paths to variables VAR0-VAR3L
 VARDL="."
-VAR0L="./experimental_docker_compose_files/00_old_one_from_proposal"
-VAR1L="./experimental_docker_compose_files/01_simple_controller_plus_agent"
-VAR2L="./experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller"
-VAR3L="./experimental_docker_compose_files/03_maven_tutorial"
+VAR1L="./experimental_docker_compose_files/00_old_one_from_proposal"
+VAR2L="./experimental_docker_compose_files/01_simple_controller_plus_agent"
+VAR3L="./experimental_docker_compose_files/02_custom_docker_file_connecting_agent_and_controller"
+VAR4L="./experimental_docker_compose_files/03_maven_tutorial"
 DOCKER_COMPOSE="docker compose"
-DOCKER_VERSION="24.0.2"
-# Function to start a tutorial based on the provided path
 
+# Function to check if running in Gitpod and modify jenkins.yaml if needed
+# I created this one yesterday but it's not needed after last PR that got merged which solves this issue 
 check_gitpod() {
-    # Check for existence of this gitpod-specific file and the ENV var.
     if [ -e /ide/bin/gitpod-code ] && [ -v GITPOD_REPO_ROOT ]; then
         echo "Gitpod detected"
-        echo "Changing URL in jenkins.yaml that supports gitpod"
+        echo "Changing URL in jenkins.yaml that supports Gitpod"
         local tutorial_path=$1
         sh $tutorial_path/gitpodURL.sh
     fi
 }
 
+# Function to check if Docker Compose is installed not checked on macOS yet
 check_docker_compose() {
     if ! command -v docker &> /dev/null
     then
-      echo "Docker is not installed. Installing it now."
-      if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            # Install Docker manually on Linux
-            # Download the Docker binary
-            curl -SL https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_VERSION.tgz  -o docker.tgz
-
-            # Extract the binary
-            tar xzvf docker.tgz
-
-            # Move the binary to the /usr/bin directory
-            sudo mv docker/* /usr/bin/
-
-            # Clean up
-            rm -r docker docker.tgz
-        elif [[ "$OSTYPE" == "darwin"* ]]; then
-            # Install Docker Desktop on macOS
-            if [[ "$(uname -m)" == "arm64" ]]; then
-                # Download the Docker Desktop disk image for M1 Macs
-                curl -SL https://desktop.docker.com/mac/stable/arm64/Docker.dmg -o Docker.dmg
-            else
-                # Download the Docker Desktop disk image for Intel-based Macs
-                curl -SL https://desktop.docker.com/mac/stable/amd64/Docker.dmg -o Docker.dmg
-            fi
-
-            # Mount the disk image
-            hdiutil attach Docker.dmg
-
-            # Run the installer script
-            /Volumes/Docker/Docker.app/Contents/MacOS/install
-
-            # Unmount the disk image
-            hdiutil detach /Volumes/Docker
-
-            # Clean up
-            rm Docker.dmg
-        else
-            echo "Couldn't Install docker, please install it manually and try again"
-            exit 1
-        fi
+      echo "Docker is not installed. Please install Docker first and then run the script again."
     fi
     if ! docker compose version &> /dev/null
     then
         if ! command -v docker-compose &> /dev/null
         then
-            echo "Installing docker compose"
-            # Steps to install docker compose
+            echo "Installing Docker Compose"
+            # Steps to install Docker Compose
             if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-                # Install Docker Compose manually on Linux
-                # Download and install Compose standalone
-                curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-                # Apply executable permissions to the standalone binary in the target path for the installation
-                chmod +x /usr/local/bin/docker-compose
+                apt-get update
+                apt-get install ca-certificates curl gnupg
+                install -m 0755 -d /etc/apt/keyrings
+                curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                chmod a+r /etc/apt/keyrings/docker.gpg
             elif [[ "$OSTYPE" == "darwin"* ]]; then
                 # Install Docker Compose on macOS without Docker Desktop
                 # Install the right docker-compose binary for your chipset from the releases page
@@ -95,47 +55,52 @@ check_docker_compose() {
                 else
                     sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose
                 fi
-
                 chmod +x /usr/local/bin/docker-compose
             else
-                echo "Couldn't Install docker compose, please install it manually and try again"
+                echo "Couldn't install Docker Compose. Please install it manually and try again."
                 exit 1
             fi
-
             DOCKER_COMPOSE=$(which docker-compose)
         else
             echo "docker-compose is already installed"
             DOCKER_COMPOSE=$(which docker-compose)
         fi
     else
-        echo "docker compose is already installed"
+        echo "Docker Compose is already installed"
         DOCKER_COMPOSE="docker compose"
     fi
-
     echo $DOCKER_COMPOSE
 }
 
-
+# Function to start a tutorial based on the provided path
 start_tutorial() {
   local tutorial_path=$1
   echo "Starting tutorial $tutorial_path"
   $DOCKER_COMPOSE -f "$tutorial_path/docker-compose.yaml" up -d
 }
 
-
+# Check Docker Compose installation
 check_docker_compose
+
 # Determine the tutorial to start based on the provided argument
-if [[ "$TUTORIAL" == "$VAR0" ]]; then
-  start_tutorial "$VAR0"
-elif [[ "$TUTORIAL" == "$VAR1" ]]; then
+if [[ "$TUTORIAL" == "$VAR1" ]]; then
   start_tutorial "$VAR1L"
 elif [[ "$TUTORIAL" == "$VAR2" ]]; then
-  check_gitpod "$VAR2L"
   start_tutorial "$VAR2L"
 elif [[ "$TUTORIAL" == "$VAR3" ]]; then
   start_tutorial "$VAR3L"
+elif [[ "$TUTORIAL" == "$VAR4" ]]; then
+  start_tutorial "$VAR4L"
 else
   # If no valid argument was passed, run the default tutorial
-  echo "No valid argument was selected, running the default tutorial"
+  echo "No valid argument was selected. Running the default tutorial"
   start_tutorial "$VARDL"
+fi
+
+# Track which tutorials have been run for teardown command
+echo "$TUTORIAL"
+if [[ -z "$TUTORIAL" ]]; then
+    echo "0" >> ./.tutorials_running.txt
+else
+  echo "$TUTORIAL" >> ./.tutorials_running.txt
 fi
