4b85ca7eb711a047ef2c3ece0dcb3be3329b592a
gounthar <gounthar@gmail.com>
2023-07-04 17:43:38 +0200

    feature(docker): Doing even better, thank you Damien!

--- 04_sidekick/keygen.sh
+++ 04_sidekick/keygen.sh
@@ -6,21 +6,26 @@ LOC=$1
 if [[ -z "$LOC" ]]; then
     LOC="./"
 fi
-echo hello
-echo $LOC
-mkdir -p $LOC/dockerfiles/secrets
+
+echo Location is $LOC
+ls -artl $LOC
+
+mkdir -p $LOC/secrets
 
 # Remove existing keys
-rm -f $LOC/dockerfiles/secrets/jenkins_agent_ed $LOC/dockerfiles/secrets/jenkins_agent_ed.pub
+rm -fr $LOC/secrets/jenkins_agent_ed $LOC/secrets/jenkins_agent_ed.pub
 
 # Generate new ed25519 SSH key pair
-ssh-keygen -t ed25519 -f $LOC/dockerfiles/secrets/jenkins_agent_ed -N ""
+ssh-keygen -t ed25519 -f $LOC/secrets/jenkins_agent_ed -N ""
 
 # Set appropriate permissions for private key
-chmod 400 $LOC/dockerfiles/secrets/jenkins_agent_ed
+chmod 400 $LOC/secrets/jenkins_agent_ed
 
 # Extract public key
-pubkey=$(cat $LOC/dockerfiles/secrets/jenkins_agent_ed.pub)
+pubkey=$(cat $LOC/secrets/jenkins_agent_ed.pub)
+
+# This file will be used by other containers to know we went up to the end of the key generation
+echo "OK" > $LOC/secrets/conductor_ok
 
 # Display success message
 echo "SSH key pair generated successfully."
