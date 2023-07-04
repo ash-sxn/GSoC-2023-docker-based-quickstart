6f311e9a671106265d41d6668662f056a124f9b3
gounthar <gounthar@gmail.com>
2023-07-04 19:15:22 +0200

    fix(docker): Works without any siouxerie.

--- 04_sidekick/keygen.sh
+++ 04_sidekick/keygen.sh
@@ -13,24 +13,23 @@ ls -artl $LOC
 mkdir -p $LOC/secrets
 
 # Remove existing keys
-rm -fr $LOC/secrets/jenkins_agent_ed $LOC/secrets/jenkins_agent_ed.pub
+rm -fr $LOC/jenkins_agent_ed $LOC/jenkins_agent_ed.pub
 
 # Generate new ed25519 SSH key pair
-ssh-keygen -t ed25519 -f $LOC/secrets/jenkins_agent_ed -N ""
+ssh-keygen -t ed25519 -f $LOC/jenkins_agent_ed -N ""
 
 # Set appropriate permissions for private key
-chmod 444 $LOC/secrets/jenkins_agent_ed
+chmod 444 $LOC/jenkins_agent_ed
 
 # Extract public key
-pubkey=$(cat $LOC/secrets/jenkins_agent_ed.pub)
+pubkey=$(cat $LOC/jenkins_agent_ed.pub)
 
 echo The public key is $pubkey
 
-# Creates the environment file that will be used by the Jenkins ssh agent
-echo "JENKINS_AGENT_SSH_PUBKEY=$pubkey" > app/secrets/jenkins_agent_ed.env
+echo "$pubkey" > $LOC/authorized_keys && chown 1000:1000 $LOC/authorized_keys
 
 # This file will be used by other containers to know we went up to the end of the key generation
-echo "OK" > $LOC/secrets/conductor_ok
+echo "OK" > $LOC/conductor_ok
 
 # Display success message
 echo "SSH key pair generated successfully."
